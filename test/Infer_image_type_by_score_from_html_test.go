288345f144bf5f01a7150c39cb2789c703ec8593
gaoyong <gaoyong06@qq.com>
2023-05-08 20:40:13 +0800

    修改：带着bug提交

--- test/Infer_image_type_by_score_from_html_test.go
+++ test/Infer_image_type_by_score_from_html_test.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -29,47 +30,57 @@ func TestInferImageTypeByScoreFromHTML(t *testing.T) {
 		panic(err)
 	}
 
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
 	// Loop through each file
 	for _, file := range fileList {
 		// Check if the file is an HTML file
 		if filepath.Ext(file.Name()) == ".html" || !strings.HasPrefix(file.Name(), updatedFilePrefix) {
-			// Open the file
-			htmlFile, err := os.Open(directoryPath + file.Name())
-			if err != nil {
-				panic(err)
-			}
-			defer htmlFile.Close()
+			// Add to the wait group
+			wg.Add(1)
+			go func(file os.FileInfo) {
+				defer wg.Done()
+				// Open the file
+				htmlFile, err := os.Open(directoryPath + file.Name())
+				if err != nil {
+					panic(err)
+				}
+				defer htmlFile.Close()
 
-			// Read the file into a string
-			htmlBytes, err := ioutil.ReadAll(htmlFile)
-			if err != nil {
-				panic(err)
-			}
-			htmlStr := string(htmlBytes)
+				// Read the file into a string
+				htmlBytes, err := ioutil.ReadAll(htmlFile)
+				if err != nil {
+					panic(err)
+				}
+				htmlStr := string(htmlBytes)
 
-			// Get image info from HTML using the GetImageInfoFromHTML function
-			imgsInfo, filteredImgs, err := spiders.InferImageTypeByScoreFromHTML(htmlStr)
-			if err != nil {
-				panic(err)
-			}
+				// Get image info from HTML using the GetImageInfoFromHTML function
+				imgsInfo, filteredImgs, err := spiders.InferImageTypeByScoreFromHTML(htmlStr)
+				if err != nil {
+					panic(err)
+				}
 
-			fmt.Printf("=================fileName: %s,  len(imgsInfo): %d, filteredImgs: %#v \n", file.Name(), len(imgsInfo), filteredImgs)
+				fmt.Printf("=================fileName: %s,  len(imgsInfo): %d, filteredImgs: %#v \n", file.Name(), len(imgsInfo), filteredImgs)
 
-			// Loop through each image to add the custom class and border
-			for _, img := range imgsInfo {
-				fmt.Printf("=================fileName: %s,  imgInfo: %#v\n", file.Name(), img)
+				// Lock the mutex
+				mu.Lock()
 
-				// Escape any regex special characters in img["src"]
-				imgSrc := regexp.QuoteMeta(img["src"].(string))
+				// Loop through each image to add the custom class and border
+				for _, img := range imgsInfo {
+					// fmt.Printf("=================fileName: %s,  imgInfo: %#v\n", file.Name(), img)
 
-				// Generate regex pattern to match the img tag with any attributes in any order
-				imgRegex := regexp.MustCompile(fmt.Sprintf(`(?i)<img.*?src=['"]%s['"].*?>`, imgSrc))
+					// Escape any regex special characters in img["src"]
+					imgSrc := regexp.QuoteMeta(img["src"].(string))
 
-				// Convert the img size from bytes to kb
-				sizeKB := fmt.Sprintf("%.2f", img["size"].(float64)/1024)
+					// Generate regex pattern to match the img tag with any attributes in any order
+					imgRegex := regexp.MustCompile(fmt.Sprintf(`(?i)<img.*?src=['"]%s['"].*?>`, imgSrc))
 
-				// Generate overlay text displaying image information in the top right corner
-				overlayText := fmt.Sprintf(`<div style="position:relative">
+					// Convert the img size from bytes to kb
+					sizeKB := fmt.Sprintf("%.2f", img["size"].(float64)/1024)
+
+					// Generate overlay text displaying image information in the top right corner
+					overlayText := fmt.Sprintf(`<div style="position:relative">
 
 								<img src="%s" class="custom-class" style="max-width: 100%%; height: auto; display: block;">
 									<div style="position:absolute; top: 0; right: 0; padding: 10px; background-color: rgba(0,255,0,0.5); color: white; font-size: 12px; font-weight: bold;">
@@ -77,27 +88,34 @@ func TestInferImageTypeByScoreFromHTML(t *testing.T) {
 									</div>
 							</div>`, img["src"], img["ratio"], img["width"], img["height"], img["format"], img["type"], img["shape"], sizeKB)
 
-				// The ReplaceAllStringFunc() method replaces each matched image tag with the above overlay text
-				htmlStr = imgRegex.ReplaceAllStringFunc(htmlStr, func(match string) string {
-					return overlayText
-				})
+					// The ReplaceAllStringFunc() method replaces each matched image tag with the above overlay text
+					htmlStr = imgRegex.ReplaceAllStringFunc(htmlStr, func(match string) string {
+						return overlayText
+					})
 
-			}
+				}
+				// Unlock the mutex
+				mu.Unlock()
 
-			// Create the updated file with a path relative to the original file
-			updatedFilePath := filepath.Dir(directoryPath+file.Name()) + "/" + updatedFilePrefix + file.Name()
-			updatedFile, err := os.Create(updatedFilePath)
-			if err != nil {
-				panic(err)
-			}
-			// Write the updated HTML string to the file
-			_, err = updatedFile.WriteString(htmlStr)
-			if err != nil {
-				panic(err)
-			}
+				// Create the updated file with a path relative to the original file
+				updatedFilePath := filepath.Dir(directoryPath+file.Name()) + "/" + updatedFilePrefix + file.Name()
+				updatedFile, err := os.Create(updatedFilePath)
+				if err != nil {
+					panic(err)
+				}
+				// Write the updated HTML string to the file
+				_, err = updatedFile.WriteString(htmlStr)
+				if err != nil {
+					panic(err)
+				}
 
-			updatedFile.Close()
-			fmt.Printf("===== success. updatedFile: %s\n", updatedFilePath)
+				updatedFile.Close()
+				fmt.Printf("===== success. updatedFile: %s\n", updatedFilePath)
+
+			}(file)
+
 		}
 	}
+	// Wait for all goroutines to finish
+	wg.Wait()
 }
