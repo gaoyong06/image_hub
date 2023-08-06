5a0633b345a85464b615bab09b6c84230d008940
gaoyong <gaoyong06@qq.com>
2023-08-06 21:50:23 +0800

    bugfix

--- merge_article_table.sh
+++ merge_article_table.sh
@@ -22,11 +22,11 @@ total_tables=$(echo "$suffix_tables" | wc -l)
 counter=0
 
 # Loop through each suffix table and insert its records into tbl_article
-for table in $suffix_tables; do
+for table in "$suffix_tables"; do
     counter=$((counter+1))
     echo "Processing table $counter of $total_tables: $table"
     
-    mysql -h $host -P $port -u $user -p$passwd $database -e "INSERT INTO tbl_article (sn, mid, idx, biz, author, wechat_id, title, tags, sections, local_path, publish_time, created_at, updated_at, deleted_at) SELECT sn, mid, idx, biz, author, wechat_id, title, tags, sections, local_path, publish_time, created_at, updated_at, deleted_at FROM $table ON DUPLICATE KEY UPDATE tbl_article.sn = VALUES(sn), tbl_article.mid = VALUES(mid), tbl_article.idx = VALUES(idx), tbl_article.biz = VALUES(biz), tbl_article.author = VALUES(author), tbl_article.wechat_id = VALUES(wechat_id), tbl_article.title = VALUES(title), tbl_article.tags = VALUES(tags), tbl_article.sections = VALUES(sections), tbl_article.local_path = VALUES(local_path), tbl_article.publish_time = VALUES(publish_time), tbl_article.created_at = VALUES(created_at), tbl_article.updated_at = VALUES(updated_at), tbl_article.deleted_at = VALUES(deleted_at);"
+    mysql -h $host -P $port -u $user -p$passwd $database -e "INSERT INTO tbl_article (sn, mid, idx, biz, author, wechat_id, title, tags, sections, local_path, publish_time, created_at, updated_at, deleted_at) SELECT sn, mid, idx, biz, author, wechat_id, title, tags, sections, local_path, publish_time, created_at, updated_at, deleted_at FROM "$table" ON DUPLICATE KEY UPDATE tbl_article.sn = VALUES(sn), tbl_article.mid = VALUES(mid), tbl_article.idx = VALUES(idx), tbl_article.biz = VALUES(biz), tbl_article.author = VALUES(author), tbl_article.wechat_id = VALUES(wechat_id), tbl_article.title = VALUES(title), tbl_article.tags = VALUES(tags), tbl_article.sections = VALUES(sections), tbl_article.local_path = VALUES(local_path), tbl_article.publish_time = VALUES(publish_time), tbl_article.created_at = VALUES(created_at), tbl_article.updated_at = VALUES(updated_at), tbl_article.deleted_at = VALUES(deleted_at);"
     
     echo "Table $counter of $total_tables: $table processed"
 done
