58eee8961f7b071cd030a5231ae41e6f315119fb
gaoyong <gaoyong06@qq.com>
2023-08-12 17:43:58 +0800

    增加：缩略图生成脚本

--- generate_thumbnails.sh
+++ generate_thumbnails.sh
@@ -6,7 +6,7 @@ original_dir="D:/work/wechat_download_data/images"
 thumbnail_dir="D:/work/wechat_download_data/thumbnails"
 
 # 遍历原图目录中的所有图片文件
-find "$original_dir" -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" | while read -r original_image; do
+find "$original_dir" -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -iname "*.webp" | while read -r original_image; do
     # 构造缩略图路径
     thumbnail_image="${original_image/$original_dir/$thumbnail_dir}"
 
@@ -18,4 +18,9 @@ find "$original_dir" -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png"
 
     # 输出生成的缩略图路径
     echo "Generated thumbnail: $thumbnail_image"
+
+    # 输出进度信息
+    processed_count=$(find "$thumbnail_dir" -type f | wc -l)
+    total_count=$(find "$original_dir" -type f -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -iname "*.webp" | wc -l)
+    echo "Progress: $processed_count / $total_count"
 done
